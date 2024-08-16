ef376b0a7e58b55680b45ee67fd5731067356cc2
S.Saha <soumabhasaha1509+github@gmail.com>
2024-08-16 12:10:01 +0530

    product master created

--- SALESMAN_MASTER.sql
+++ SALESMAN_MASTER.sql
@@ -1,3 +1,4 @@
+drop table if exists SALESMAN_MASTER;
 create table SALESMAN_MASTER (
     SALESMAN_NO varchar2(6),
     SALESMAN_NAME varchar2(20),
@@ -11,4 +12,60 @@ create table SALESMAN_MASTER (
     YTD_SALES number(6,2),
     REMARKS varchar2(60)
 );
+set linesize 100;
 desc SALESMAN_MASTER;
+insert into SALESMAN_MASTER values (
+    'S00001',
+    'Aman',
+    'A/14',
+    'Worli',
+    'Mumbai',
+    400002,
+    'Maharashtra',
+    10000.00,
+    5000.00,
+    2000.00,
+    'Good Salesman'
+);
+insert into SALESMAN_MASTER values (
+    'S00002',
+    'Omkar',
+    '65',
+    'Niriman',
+    'Mumbai',
+    400001,
+    'Maharashtra',
+    12000.00,
+    6000.00,
+    3000.00,
+    'Excellent Salesman'
+);
+insert into SALESMAN_MASTER values (
+    'S00003',
+    'Raj',
+    'P-7',
+    'Bandra',
+    'Mumbai',
+    400032,
+    'Maharashtra',
+    15000.00,
+    7000.00,
+    4000.00,
+    'Outstanding Salesman'
+);
+insert into SALESMAN_MASTER values (
+    'S00004',
+    'Ashish',
+    'A/5',
+    'Juhu',
+    'Mumbai',
+    400044,
+    'Maharashtra',
+    15000.00,
+    7000.00,
+    4000.00,
+    'Nice Salesman'
+);
+set linesize 400;
+select * from SALESMAN_MASTER;
+commit;
